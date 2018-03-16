f0fbd6d6e82ce732cc3b748386ca043795cc2085
Derek Pockrandt <derekpock@gmail.com>
2018-03-16 00:37:53 -0500

    Modified the create disk program to be quicker, included the retval prog

--- src/userprog/createDisk.sh
+++ src/userprog/createDisk.sh
@@ -3,9 +3,10 @@ make clean
 make
 cd build
 pintos-mkdisk filesys.dsk --filesys-size=2
-pintos -f -q
-pintos -p ../../examples/echo -a echo -- -q
-pintos -p ../../examples/pwd -a pwd -- -q
-pintos -p ../../examples/test -a test -- -q
+pintos -v -- -f -q
+pintos -v -p ../../examples/echo -a echo -- -q
+pintos -v -p ../../examples/pwd -a pwd -- -q
+pintos -v -p ../../examples/test -a test -- -q
+pintos -v -p ../../examples/retval -a retval -- -q
 echo "Now do 'pintos -v -- run 'test'' or any other executable added."
 cd ..
