3904205be86936b89bdbbf18470ed0044132ceb4
Derek Pockrandt <derekpock@gmail.com>
2018-03-16 03:08:50 -0500

    Pushing filesys.dsk and updated error showing createDisk script

--- src/userprog/createDisk.sh
+++ src/userprog/createDisk.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-make clean
-make
-cd build
-pintos-mkdisk filesys.dsk --filesys-size=2
-pintos -v -- -f -q
-pintos -v -p ../../examples/echo -a echo -- -q
-pintos -v -p ../../examples/pwd -a pwd -- -q
-pintos -v -p ../../examples/test -a test -- -q
-pintos -v -p ../../examples/retval -a retval -- -q
+inBuild=0 &&
+make clean &&
+make &&
+cd build &&
+inBuild=1 &&
+pintos-mkdisk filesys.dsk --filesys-size=2 &&
+pintos -v -- -f -q &&
+pintos -v -p ../../examples/echo -a echo -- -q &&
+pintos -v -p ../../examples/pwd -a pwd -- -q &&
+pintos -v -p ../../examples/test -a test -- -q &&
+pintos -v -p ../../examples/retval -a retval -- -q &&
+cp filesys.dsk ../ &&
 echo "Now do 'pintos -v -- run 'test'' or any other executable added."
-cd ..
+retVal=$?
+if [[ $inBuild == 1 ]]
+then
+	cd ..
+fi
+exit $retVal
